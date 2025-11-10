0f70721e070a61a6aa7393632a2bdf69f53b29c6
NicMen99 <niccolo.menghini@stud.unifi.it>
2025-11-10 23:43:57 +0100

    First commit: adding maven generated files and pom.xml with all requested libraries

---
+++ src/main/java/org/swam/sirio_mcp_server/SirioMcpServerApplication.java
@@ -0,0 +1,13 @@
+package org.swam.sirio_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirioMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SirioMcpServerApplication.class, args);
+    }
+
+}
