da3548431ecc980a76e6a0954bb87d62b39eb311
FSciamma <filipposciammacca@gmail.com>
2025-11-11 17:54:48 +0100

    Fixato bug per addprecondition e addpostcondition

--- src/main/java/org/swam/sirio_mcp_server/SirioService.java
+++ src/main/java/org/swam/sirio_mcp_server/SirioService.java
@@ -2,6 +2,7 @@ package org.swam.sirio_mcp_server;
 
 import org.oristool.petrinet.*;
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -36,16 +37,44 @@ public class SirioService {
     }
 
     @Tool(name = "add_precondition", description = "Add a precondition to a transition")
-    public PetriNet addPrecondition(Place p, Transition t) {
+    public PetriNet addPrecondition(
+        @ToolParam(description = "Name of the place") String place_name,
+        @ToolParam(description = "Name of the transition") String transition_name
+    ){
+        // Trova il place per nome 
+        Place p = petriNet.getPlaces().stream()
+            .filter(place -> place.getName().equals(place_name))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Place not found" + place_name));
+        
+
+        // Trova la transition per nome
+        Transition t = petriNet.getTransitions().stream()
+            .filter(trans -> trans.getName().equals(transition_name))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Transition not found" + transition_name));
+
         petriNet.addPrecondition(p, t);
         return petriNet;
     }
 
     @Tool(name = "add_postcondition", description = "Add a postcondition to a transition")
-    public PetriNet addPostcondition(Transition t, Place p) {
+    public PetriNet addPostcondition(
+        @ToolParam(description="Name of the transition") String transition_name,
+        @ToolParam(description="Name of the place") String place_name
+    ) {
+        // Trova la transition per nome
+        Transition t = petriNet.getTransitions().stream()
+            .filter(trans -> trans.getName().equals(transition_name))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Transition not found" + transition_name));
+
+        // Trova il place per nome 
+        Place p = petriNet.getPlaces().stream()
+            .filter(place -> place.getName().equals(place_name))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Place not found" + place_name));
         petriNet.addPostcondition(t, p);
         return petriNet;
     }
-
-
 }
